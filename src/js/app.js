0002de66552a1a448929794ec7fcf91768e54356
SheikhRashed <devrashed06@gmail.com>
2021-10-24 23:20:43 +0600

    challenge two complete

--- src/js/app.js
+++ src/js/app.js
@@ -114,13 +114,25 @@ let data2 = [16, 6, 10, 5, 6, 1, 4];
 // console.log(calcAverageHumanAge(data1));
 
 const calcAverageHumanAge = function (ages) {
-	console.log(ages);
+	// console.log(ages);
 
 	const humanAges = ages.map((age) => {
 		return age <= 2 ? 2 * age : 16 + age * 4;
 	});
 	// 1. Human Ages Finded
-	console.log(humanAges);
+	// console.log(humanAges);
+
+	// 2.Find Adults
+	const adults = humanAges.filter((age) => age >= 18);
+	// console.log(adults);
+
+	// 3. Average Age
+	const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+	return Math.floor(average);
 };
 
-calcAverageHumanAge(data1);
+let averageAgeOne = calcAverageHumanAge(data1);
+let averageAgeTwo = calcAverageHumanAge(data2);
+
+console.log(averageAgeOne, averageAgeTwo);
