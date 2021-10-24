a9aef9741a5bf296467ffdcb64609586d9eb095e
SheikhRashed <devrashed06@gmail.com>
2021-10-24 21:26:03 +0600

    removed challange code 1

--- src/js/app.js
+++ src/js/app.js
@@ -8,36 +8,37 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+	owner: 'Jonas Schmedtmann',
+	movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+	interestRate: 1.2, // %
+	pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+	owner: 'Jessica Davis',
+	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+	interestRate: 1.5,
+	pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+	owner: 'Steven Thomas Williams',
+	movements: [200, -200, 340, -300, -20, 50, 400, -460],
+	interestRate: 0.7,
+	pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+	owner: 'Sarah Smith',
+	movements: [430, 1000, 700, 50, 90],
+	interestRate: 1,
+	pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
 
 // Elements
+
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.bala  nce__value');
@@ -45,16 +46,13 @@ const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
-
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
-
 const btnLogin = document.querySelector('.login__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
-
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
@@ -64,51 +62,33 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements) {
-  containerMovements.innerHTML = '';
+	containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">
-        ${i + 1} ${type}
+	movements.forEach(function (mov, i) {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
+		const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">
+          ${i + 1} ${type}
+        </div>
+        <div class="movements__value">${mov}€</div>
       </div>
-      <div class="movements__value">${mov}€</div>
-    </div>
     `;
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
 };
 
 displayMovements(account1.movements);
 
 const createUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map((name) => name[0])
-      .join('');
-  });
+	accs.forEach(function (acc) {
+		acc.username = acc.owner
+			.toLowerCase()
+			.split(' ')
+			.map((name) => name[0])
+			.join('');
+	});
 };
 
 createUsernames(accounts);
 console.log(accounts);
-// Challenge #01
-// const juliaData = [3, 5, 2, 12, 7];
-// const kateData = [4, 1, 15, 8, 3];
-// let newJuliaData = [...juliaData];
-// // 1
-// console.log(newJuliaData.slice(1, -1));
-
-// let bothArray = [...juliaData, ...kateData];
-
-// // console.log(bothArray);
-// bothArray.forEach((age, i) => {
-//   // age >= 5 ? console.log(`Dog Number ${i} is ${age >= 5 ? 'adult' : 'puppy'} and is ${age} years old  `) : console.log('')
-//   if (age >= 5) {
-//     console.log(`Dog Number ${i + 1} is a Adult and is ${age} years old`);
-//   } else {
-//     console.log(`Dog Number ${i + 1} is a Puppy and is ${age} years old`);
-//   }
-// });
