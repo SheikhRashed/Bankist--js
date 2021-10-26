36ea4b9693bbc07f74247c38db96dd559a831f93
SheikhRashed <devrashed06@gmail.com>
2021-10-26 16:24:09 +0600

    Removed code " challenge 3 solved"
    This reverts commit 551bfceee311ee0c3b35724c066b30693c222a72.

--- src/js/app.js
+++ src/js/app.js
@@ -116,22 +116,3 @@ const calcPrintBalance = (movements) => {
 };
 
 calcPrintBalance(account2.movements);
-
-// Challenge #03
-
-// Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
-
-// TEST Data 1: [5,2,4,1,15,8,3]
-// TEST Data 2: [16,6,10,5,6,1,4]
-
-const calcAverageHumanAge = (ages) =>
-  ages
-    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter((adult) => adult >= 18)
-    .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
-let output;
-
-output = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-
-console.log(output);
