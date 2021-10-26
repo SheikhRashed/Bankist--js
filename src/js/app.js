831d0e25baea6c86a1e48a9e17f1cce7f4e2b52a
SheikhRashed <devrashed06@gmail.com>
2021-10-26 12:24:40 +0600

    UI Responsive fixed

--- src/js/app.js
+++ src/js/app.js
@@ -65,12 +65,14 @@ const displayMovements = function (movements) {
 
   movements.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
     const html = `
 		<div class="movements__row">
 			<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
 			<div class="movements__value">${mov}€</div>
 		</div>
 		`;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
