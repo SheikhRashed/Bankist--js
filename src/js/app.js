5bcfe089d52839420072dfb846ec695b5754d8a1
SheikhRashed <devrashed06@gmail.com>
2021-10-31 09:17:20 +0600

    load request implemented

--- src/js/app.js
+++ src/js/app.js
@@ -180,6 +180,22 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// Loan Request
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some((mov) => mov >= amount * 0.1)) {
+    // add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
